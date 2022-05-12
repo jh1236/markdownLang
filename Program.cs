814e494b85bbc11ac2d0f944f39e9db3a7aca509
Jared Healy <healy_jared@yahoo.com>
2022-05-12 22:26:47 +0800

    initial

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace markdownLang;
+
+class MainClass {
+    public static void Main(String[] args) {
+        Console.WriteLine("YOSDUASDAS");
+    }
+}
